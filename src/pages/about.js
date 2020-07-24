57d73232a8f9f2840b764dafd0e4760bbba9c64f
Sam Allen <allensam88@gmail.com>
2020-07-24 11:52:21 -0700

    step 3 complete

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,17 @@
 import React from "react"
+import Layout from "../components/layout"
 import Header from "../components/header"
 
 export default function About() {
   return (
-    <div style={{ color: `teal` }}>
+    <Layout>
       <Header headerText="About Gatsby" />
       <Header headerText="It's pretty cool" />
       <p>Such wow. Very React.</p>
-    </div>
+			<div>
+				<h1>About me</h1>
+				<p>I’m good enough, I’m smart enough, and gosh darn it, people like me!</p>
+			</div>
+    </Layout>
   )
 }
